0ad90c8f81002fdfd64b04dcea10ce3d3ef8a9c3
Dim L. Feiron <norefle@outlook.com>
2016-12-01 17:59:36 +0100

    Show posters accordingly to mockup.

--- app/src/main/java/com/example/norefle/movies/MoviesListFragment.java
+++ app/src/main/java/com/example/norefle/movies/MoviesListFragment.java
@@ -6,7 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.GridView;
 
 import java.util.Arrays;
 
@@ -28,15 +28,24 @@ public class MoviesListFragment extends Fragment {
         final Uri[] images = {
                 Uri.parse("http://contentmarketinginstitute.com/wp-content/uploads/2015/08/original-jaws-poster-image-1A.png"),
                 Uri.parse("http://graphicdesignjunction.com/wp-content/uploads/2011/12/grey-movie-poster.jpg"),
-                Uri.parse("http://thumbs1.ebaystatic.com/images/g/pw4AAOSw4shX5zOd/s-l225.jpg")
+                Uri.parse("http://thumbs1.ebaystatic.com/images/g/pw4AAOSw4shX5zOd/s-l225.jpg"),
+                Uri.parse("https://67.media.tumblr.com/f5e67c90ac9cec324ab80790088152e9/tumblr_of2srkrLLt1vzqmg1o1_400.gif"),
+                Uri.parse("http://contentmarketinginstitute.com/wp-content/uploads/2015/08/original-jaws-poster-image-1A.png"),
+                Uri.parse("http://graphicdesignjunction.com/wp-content/uploads/2011/12/grey-movie-poster.jpg"),
+                Uri.parse("http://thumbs1.ebaystatic.com/images/g/pw4AAOSw4shX5zOd/s-l225.jpg"),
+                Uri.parse("https://67.media.tumblr.com/f5e67c90ac9cec324ab80790088152e9/tumblr_of2srkrLLt1vzqmg1o1_400.gif"),
+                Uri.parse("http://contentmarketinginstitute.com/wp-content/uploads/2015/08/original-jaws-poster-image-1A.png"),
+                Uri.parse("http://graphicdesignjunction.com/wp-content/uploads/2011/12/grey-movie-poster.jpg"),
+                Uri.parse("http://thumbs1.ebaystatic.com/images/g/pw4AAOSw4shX5zOd/s-l225.jpg"),
+                Uri.parse("https://67.media.tumblr.com/f5e67c90ac9cec324ab80790088152e9/tumblr_of2srkrLLt1vzqmg1o1_400.gif")
 
         };
 
         View root = inflater.inflate(R.layout.fragment_movies_list, container, false);
 
         posterAdapter = new PostersAdapter(getActivity(), Arrays.asList(images));
-        ListView list = (ListView) root.findViewById(R.id.movies_list_view);
-        list.setAdapter(posterAdapter);
+        GridView movies = (GridView) root.findViewById(R.id.movies_collection_view);
+        movies.setAdapter(posterAdapter);
 
         return root;
     }
